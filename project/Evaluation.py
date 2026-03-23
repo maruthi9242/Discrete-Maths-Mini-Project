2ddefde536cac9e6f7a3f378111d1f324e2cad00
maruthi9242 <srikarhitman45@gmail.com>
2026-03-23 12:52:44 +0530

    Update Evaluation.py

--- project/Evaluation.py
+++ project/Evaluation.py
@@ -1,6 +1,3 @@
-from ai import Ai
-import re
-from sympy import symbols,sympify
 class Converstion:
     def __init__(self):
         ai=Ai()
@@ -12,19 +9,17 @@ class Converstion:
         symbols_dict= {'P': P, 'Q': Q, 'R': R}
         self.expr=sympify(self.expr,locals=symbols_dict)
 
-        if(self.expr.subs({P: False, Q: False, R: False})and
-        self.expr.subs({P: False, Q: False, R: True})and
-        self.expr.subs({P: False, Q:True , R: False})and
-        self.expr.subs({P: False, Q: True, R: True})and
-        self.expr.subs({P: True, Q: False, R: False}) and
-        self.expr.subs({P: True, Q: False, R: True}) and
-        self.expr.subs({P: True, Q: True, R: False}) and
-        self.expr.subs({P: True, Q: True, R: True})):
-            print("Its a tautology!!!\n")
-        else:
-            print("Its  not a tautology\n")
-obj=Converstion()
-obj.convert()
+
+        values=[self.expr.subs({P: False, Q: False, R: False}),
+        self.expr.subs({P: False, Q: False, R: True}),
+        self.expr.subs({P: False, Q:True , R: False}),
+        self.expr.subs({P: False, Q: True, R: True}),
+        self.expr.subs({P: True, Q: False, R: False}),
+        self.expr.subs({P: True, Q: False, R: True}),
+        self.expr.subs({P: True, Q: True, R: False}),
+        self.expr.subs({P: True, Q: True, R: True})]
+        return values
+
 
 
 
